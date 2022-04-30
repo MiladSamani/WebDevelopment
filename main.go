c3ed381eada14a501be754db9a8e7cde4f8a7943
“Milad <Eng.Milad.Samani@gmail.com>
2022-04-30 10:57:10 +0430

    adding controllers to our projects

--- main.go
+++ main.go
@@ -14,26 +14,38 @@ import (
 
 var HomeView *views.View
 var ContactView *views.View
+var SignView *views.View
 
 func main() {
 	HomeView = views.NewView("bootstrap", "views/home.gohtml")
 	ContactView = views.NewView("bootstrap", "views/contact.gohtml")
+	SignView = views.NewView("bootstrap", "views/signup.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/signup", signup)
 	err := http.ListenAndServe(":3000", r)
 	if err != nil {
 		return
 	}
 }
 
+// Home handler
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(HomeView.Render(w, nil))
 }
+
+// Contact handler
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	must(ContactView.Render(w, nil))
+}
+
+// Sign handler
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(SignView.Render(w, nil))
 }
 
 // A helper function that panics on any error
